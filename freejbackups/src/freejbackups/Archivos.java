166b98cd217c85120126864224015053ad442704
adrian <adrian@mintdebi>
2014-08-07 17:45:40 -0300

    agregando consola para planes .. (licencia GPL incluida en el proyecto)

--- freejbackups/src/freejbackups/Archivos.java
+++ freejbackups/src/freejbackups/Archivos.java
@@ -1,3 +1,22 @@
+/**
+ *
+ * @Copyright 2014 "freejbakcups" por Adrian Seimandi - bajosega@gmail.com 
+ *  
+ * This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.  
+ */
+
+
 package freejbackups;
 
 import java.io.File;
@@ -11,10 +30,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-/**
- *
- * @"freejbakcups" por Adrian Seimandi - bajosega@gmail.com - @2014
- */
+
 
 public class Archivos {
     
--- freejbackups/src/freejbackups/PlanEjecutar.java
+++ freejbackups/src/freejbackups/PlanEjecutar.java
@@ -1,12 +1,24 @@
+/**
+ *
+ * @Copyright 2014 "freejbakcups" por Adrian Seimandi - bajosega@gmail.com 
+ *  
+ * This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.  
+ */
 package freejbackups;
 
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @"freejbakcups" por Adrian Seimandi - bajosega@gmail.com - @2014
- */
- 
 public class PlanEjecutar extends javax.swing.JFrame {
 hilo hilo1 = new hilo("hilo 1");
 
